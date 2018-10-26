69d62f42297e24bfe80ae0f47ecee1a2c262a943
shimengran <smr@qinqinxiaobao.com>
2018-10-26 10:40:36 +0800

    'python模块练习'

---
+++ py_package_learn/__init__.py
@@ -0,0 +1,5 @@
+# https://www.cnblogs.com/Lands-ljk/p/5880483.html
+# 可以在 __init__.py中导入模块
+# 在包内使用时，可以直接 package_name.module_name.method_name 进行使用
+# 例如 py_package_learn.os.path
+import os
---
+++ py_package_learn/package_learn.py
@@ -0,0 +1,4 @@
+import py_package_learn
+
+if __name__ == '__main__':
+    print(py_package_learn.os.environ)
