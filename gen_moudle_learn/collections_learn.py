8b446110548576954a48c453fa65437bd360765b
shimengran <smr@qinqinxiaobao.com>
2018-10-26 14:43:19 +0800

    collections内建模块学习完成

--- gen_moudle_learn/collections_learn.py
+++ gen_moudle_learn/collections_learn.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from collections import namedtuple, deque, defaultdict, OrderedDict
+from collections import namedtuple, deque, defaultdict, OrderedDict, ChainMap, Counter
 
 
 def namedtuple_learn():
@@ -31,11 +31,13 @@ def defaultdict_learn():
     print(dd['name'])
 
 
-def orderedDict_learn():
+def ordereddict_learn():
     # 默认字典是按照key排序的
     # OrderedDict是按照插入顺序排序的
+    # 搜索
     od = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
     print(od)
+
 
 # FIFO_DICT
 class LastUpdatedOrderedDict(OrderedDict):
@@ -56,14 +58,30 @@ class LastUpdatedOrderedDict(OrderedDict):
             print('add:', (key, value))
         OrderedDict.__setitem__(self, key, value)
 
-def chainMap_learn():
+
+def chainmap_learn():
     # ChainMap可以把一组dict串起来并组成一个逻辑上的dict。
     # ChainMap本身也是一个dict，但是查找的时候，会按照顺序在内部的dict依次查找。
-    pass
+    cm = ChainMap({'name': 'john', 'age': 20}, {'mobile': '17600104924'})
+    print(cm)
+    print(cm['name'])
+    print(cm['mobile'])
+
+
+def count_learn():
+    names = ['john', 'siri', 'air', 'dick', 'dick', 'fuck', 'what']
+    c = Counter()
+    for name in names:
+        c[name] += 1
+    print(type(c), c)
+    print(type(c.values()), c.values())
+
 
 if __name__ == '__main__':
     # namedtuple_learn()
     # deque_learn()
     # defaultdict_learn()
     # orderedDict_learn()
+    # chainmap_learn()
+    # count_learn()
     pass
