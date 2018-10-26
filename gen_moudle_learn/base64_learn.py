6a6c6e90fbc9293b9829b5215a7391f96c1eb674
shimengran <smr@qinqinxiaobao.com>
2018-10-26 14:57:51 +0800

    添加字符串与字节数组互换方法

--- gen_moudle_learn/base64_learn.py
+++ gen_moudle_learn/base64_learn.py
@@ -7,3 +7,17 @@ if __name__ == '__main__':
     print(type(decode), decode)
     bytes_decode = bytes.decode(decode)
     print(type(bytes_decode), bytes_decode)
+
+    # bytes object
+    b = b"example"
+    # str object
+    s = "example"
+    # str to bytes
+    bytes(s, encoding="utf8")
+    # bytes to str
+    str(b, encoding="utf-8")
+    # an alternative method
+    # str to bytes
+    str.encode(s)
+    # bytes to str
+    bytes.decode(b)
